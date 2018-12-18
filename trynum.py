464ad363cfb2b3f05991eb52882cb5036375fc48
takedarut <takedarut@nttdata.co.jp>
2018-12-18 20:05:12 +0900

    no1

---
+++ trynum.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+x = np.array([1.0, 2.0, 3.0])
+#print(x)
+
+y = np.array([2.0, 4.0, 6.0])
+
+#print(x+y)
+#print(x*y)
+#print(x/y)
+#print(x/2.0)
+
+A = np.array([[1,2],[3,4]])
+#print(A)
+
+#print(A.shape)
+#print(A.dtype)
+
+B = np.array([[3,0],[0,6]]) 
+print(A*B)
