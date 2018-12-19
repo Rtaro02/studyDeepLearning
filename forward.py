a905e7a50e4b06637ceab26f13d0c7eb68f997d8
Rtaro02 <takedarut@nttdata.co.jp>
2018-12-19 12:59:44 +0900

    day2

---
+++ forward.py
@@ -0,0 +1,11 @@
+def identify_function(x):
+    return x
+def init_network():
+    network = {}
+    network['W1'] = np.array([0.1,0.3,0.5], [0.2,0.4,0.6])
+    network['b1'] = np.array([0.1,0.2,0.3])
+    network['W1'] = np.array([0.1,0.3,0.5], [0.2,0.4,0.6])
+    network['W1'] = np.array([0.1,0.3,0.5], [0.2,0.4,0.6])
+    network['W2'] = np.array([0.1,0.3,0.5], [0.2,0.4,0.6])
+    network['W3'] = np.array([0.1,0.3,0.5], [0.2,0.4,0.6])
+    
